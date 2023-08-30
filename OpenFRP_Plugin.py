c8e94043d90fe9e566c5c11f467fe30b636752cc
LxHTT <lxhtz.dl@qq.com>
2023-08-30 18:25:01 +0800

    A | 初始化插件

---
+++ OpenFRP_Plugin.py
@@ -0,0 +1,70 @@
+from Adapters.Plugin import Plugin
+from MCSL2Lib.windowInterface import Window
+from PyQt5.QtCore import Qt
+from .Interfaces.OfInterface import OpenFrpMainUI
+from qfluentwidgets import FluentIcon as FIF, InfoBar, InfoBarPosition
+OpenFRP_Plugin = Plugin()
+
+ofInterface = OpenFrpMainUI()
+
+def load():
+    pass
+
+
+def enable():
+    try:
+        Window().addSubInterface(ofInterface, FIF.WIFI, "OpenFrp")
+        InfoBar.success(
+            title="提示",
+            content="OpenFrp 插件已启用。",
+            orient=Qt.Horizontal,
+            isClosable=True,
+            position=InfoBarPosition.BOTTOM_LEFT,
+            duration=2500,
+            parent=Window().pluginsInterface,
+        )
+    except Exception as e:
+        InfoBar.error(
+            title="提示",
+            content=f"OpenFrp 插件启用失败！\n{e}",
+            orient=Qt.Horizontal,
+            isClosable=True,
+            position=InfoBarPosition.BOTTOM_LEFT,
+            duration=2500,
+            parent=Window().pluginsInterface,
+        )
+
+
+def disable():
+    try:
+        Window().navigationInterface.removeWidget(routeKey=ofInterface.objectName())
+        ofInterface.setParent(None)
+        InfoBar.success(
+            title="提示",
+            content="OpenFrp 插件已禁用。",
+            orient=Qt.Horizontal,
+            isClosable=True,
+            position=InfoBarPosition.BOTTOM_LEFT,
+            duration=2500,
+            parent=Window().pluginsInterface,
+        )
+    except Exception as e:
+        InfoBar.error(
+            title="提示",
+            content=f"OpenFrp 插件禁用失败！\n{e}",
+            orient=Qt.Horizontal,
+            isClosable=True,
+            position=InfoBarPosition.BOTTOM_LEFT,
+            duration=2500,
+            parent=Window().pluginsInterface,
+        )
+
+
+# 注册加载代码
+OpenFRP_Plugin.register_loadFunc(load)
+
+# 注册应用代码
+OpenFRP_Plugin.register_enableFunc(enable)
+
+# 注册应用代码
+OpenFRP_Plugin.register_disableFunc(disable)
---
+++ README.md
@@ -0,0 +1,17 @@
+![大头图](https://socialify.git.ci/MCSLTeam/OpenFRP_Plugin/image?description=1&font=Jost&language=1&owner=1&pattern=Circuit%20Board&theme=Auto&logo=https://img.fastmirror.net/s/2023/07/17/64b5471e1d478.png)
+[![](https://img.shields.io/github/v/tag/MCSLTeam/OpenFRP_Plugin?label=ver&style=for-the-badge "Version")](https://github.com/MCSLTeam/OpenFRP_Plugin/releases/latest)   [![](https://img.shields.io/github/downloads/MCSLTeam/OpenFRP_Plugin/total?style=for-the-badge "Downloads")](https://github.com/MCSLTeam/OpenFRP_Plugin/releases)  [![](https://img.shields.io/github/license/MCSLTeam/OpenFRP_Plugin?style=for-the-badge "License")](https://github.com/MCSLTeam/OpenFRP_Plugin/blob/master/LICENSE)
+### 前言
+这是一个可以在MCSL2上使用OpenFRP功能的插件，需要你的OpenFRP账号密码，可以提供服务器的内网穿透功能。  
+为MCSL2量身打造。  
+支持MCSL2 2.2.1.0及以上版本。  
+### 关于账密安全  
+OpenFRP OPENAPI调用前提必须使用账密登录，登录全程不会上传MCSL2。账密、Cookie等均保存在本地，MCSL2不会泄露。  
+### 安装方法  
+#### 下载插件  
+先[点击此处](https://github.com/MCSLTeam/OpenFRP_Plugin/releases)下载最新版插件。  
+#### 解压  
+解压压缩包。  
+#### 安装  
+打开MCSL2插件页面，点击“安装插件”，选择此插件的压缩包。安装后，请重启MCSL2。  
+#### 启用  
+在MCSL2的`插件`页面启用此插件，然后，你就会在菜单栏上找到此插件了。
